8a0b3c3dc88849ea95067434219baed9a2f64759
Kai <henning.kai@web.de>
2024-09-01 00:11:28 +0200

    update

--- train_gkn.py
+++ train_gkn.py
@@ -1,3 +1,5 @@
+import sys
+import time
 import torch.nn.functional as F
 from torch_geometric.loader import DataLoader
 
@@ -5,6 +7,7 @@ from hlp.hdf5 import load_pde_dataset
 
 from gkn.utilities import *
 from gkn.nn_conv import NNConv_old, NNConv, NNConv_Gaussian
+
 
 class KernelNN(torch.nn.Module):
     def __init__(self, width, ker_width, depth, ker_in, in_width=1, out_width=1):
@@ -28,43 +31,57 @@ class KernelNN(torch.nn.Module):
         return x
 
 
-width = 32
-ker_width = 32
-depth = 6
-edge_features = 8
-node_features = 7
-batch_size = 2
-epochs = 100
-learning_rate = 0.005
-scheduler_step = 50
-scheduler_gamma = 0.5
+if __name__== "__main__":
+    time_restrict=True
+    max_time_in_hours = 0.1
+    start = time.time()
 
-model = KernelNN(width, ker_width, depth, edge_features, in_width=node_features)
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=5e-4)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=scheduler_step, gamma=scheduler_gamma)
-myloss = LpLoss(size_average=False)
+    dataset_path = 'data/train_data.h5'
 
-train_data = load_pde_dataset('data/train_data.h5')
-train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    width = 32
+    ker_width = 32
+    depth = 6
+    edge_features = 8
+    node_features = 7
+    batch_size = 2
+    epochs = 100
+    learning_rate = 0.005
+    scheduler_step = 50
+    scheduler_gamma = 0.5
+
+    model = KernelNN(width, ker_width, depth, edge_features, in_width=node_features)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=5e-4)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=scheduler_step, gamma=scheduler_gamma)
+    myloss = LpLoss(size_average=False)
+
+    train_data = load_pde_dataset(dataset_path)
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
 
 
-model.train()
+    model.train()
 
-for epochn in range(epochs):
-    train_mse = 0.0
-    for batch in train_loader:
-        optimizer.zero_grad()
-        out = model(batch)
-        out_np = out.view(-1, 1).detach().cpu().numpy()
+    for epochn in range(epochs):
+        train_mse = 0.0
+        for batch in train_loader:
+            optimizer.zero_grad()
+            out = model(batch)
+            out_np = out.view(-1, 1).detach().cpu().numpy()
 
-        mse = F.mse_loss(out.view(-1, 1), batch.y.view(-1,1))
-        mse.backward()
-        optimizer.step()
-        train_mse += mse.item()
+            mse = F.mse_loss(out.view(-1, 1), batch.y.view(-1,1))
+            mse.backward()
+            optimizer.step()
+            train_mse += mse.item()
 
-    print(train_mse/len(train_loader))
-    scheduler.step()
-    model.eval()
+            end = time.time()
+            diff = end - start
+            diff_h = diff/3600.
+            print(f'remaining time : {diff_h}')
+            if(diff_h >= max_time_in_hours and time_restrict==True):
+                sys.exit(0)
 
-torch.save(model.state_dict(), "data/current_model.pt")
+        print(train_mse/len(train_loader))
+        scheduler.step()
+        model.eval()
+
+    torch.save(model.state_dict(), "data/current_model.pt")
 
